99b65349426de109b520c9ab68ea132f082c41e9
Junichi Sato <sat0b.3ee@gmail.com>
2025-07-28 13:47:46 +0900

    Add diagflog settings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -225,6 +225,38 @@ return {
     end,
   },
 
+  {
+    "dgagn/diagflow.nvim",
+    event = "LspAttach",
+    opts = {
+      enable = true,
+      max_width = 60,
+      max_height = 10,
+      severity_colors = {
+        error = "DiagnosticFloatingError",
+        warning = "DiagnosticFloatingWarn",
+        info = "DiagnosticFloatingInfo",
+        hint = "DiagnosticFloatingHint",
+      },
+      format = function(diagnostic)
+        return diagnostic.message
+      end,
+      gap_size = 1,
+      scope = 'cursor',
+      padding_top = 0,
+      padding_right = 0,
+      text_align = 'left',
+      placement = 'inline',
+      inline_padding_left = 3,
+      update_event = { 'DiagnosticChanged', 'BufReadPost' },
+      toggle_event = {},
+      show_sign = false,
+      render_delay = 125,
+      display_max_lines = 3,
+      show_borders = false,
+    },
+  },
+
 
   {
   	"nvim-treesitter/nvim-treesitter",
